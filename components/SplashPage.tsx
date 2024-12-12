250d2e85f3306e2a034e3a0c12aa11296f95b903
Vivek Misra <115539722+vimis22@users.noreply.github.com>
2024-12-12 19:28:36 +0100

    Emulator is as before, still working fine after adding firebase-app and auth dependencies. Some references have also been fixed.

--- components/SplashPage.tsx
+++ components/SplashPage.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, StyleSheet, TouchableOpacity} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const SplashPage = ({ navigation }: any) => {
+    useEffect(() => {
+        const checkUserLogin = async () => {
+            return auth().onAuthStateChanged(user => {
+                if (user) {
+                    navigation.replace('RoomlistPage'); // Naviger til RoomlistScreen hvis logget ind
+                } else {
+                    navigation.replace('LoginPage'); // Naviger til LoginScreen hvis ikke logget ind
+                }
+            });
+        };
+
+        checkUserLogin();
+    }, [navigation]);
     return (
         <TouchableOpacity style={styles.pageContainer} onPress={() => navigation.navigate('LoginPage')}>
             <Text style={styles.welcomeTitle}>Chentia</Text>
