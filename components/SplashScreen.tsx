7691e0eb1f39360bdb798cf4f3e521d0b219659c
Vivek Misra <115539722+vimis22@users.noreply.github.com>
2024-12-12 13:33:10 +0100

    The Emulator is working after adding GestureHandler and StackScreen on Console. The Navigation is working.

---
+++ components/SplashScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const SplashScreen = ({ navigation }: any) => {
+    return (
+        <TouchableOpacity style={styles.pageContainer} onPress={() => navigation.navigate('LoginScreen')}>
+            <Text style={styles.welcomeTitle}>Chentia</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    pageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#330099',
+    },
+    welcomeTitle: {
+        fontSize: 32,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
+
+export default SplashScreen;
